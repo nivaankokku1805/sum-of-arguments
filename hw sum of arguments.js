43314ec15bc2c7d8d6849d7ad0ccb2121b324a1f
nivaankokku1805 <reetu18@gmail.com>
2025-05-30 12:20:17 +0530

    Add files via upload

---
+++ hw sum of arguments.js
@@ -0,0 +1,8 @@
+var value = Math.sin(45);
+document.write("Sin 45 is :" + value + "<br>");
+var value = Math.cos(45);
+document.write("Cos 45 is :" + value1 + "<br>");
+var value = Math.tan(45);
+document.write("Tan 45 is :" + value2 + "<br>");
+
+
